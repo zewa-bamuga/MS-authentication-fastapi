35a4575387cfa28c6d1074e0a5e5c32b158afe48
FEL <Vovafelinger75@gmail.com>
2025-03-17 00:08:16 +0700

    added traefic to deploy

--- src/app/fastapi.py
+++ src/app/fastapi.py
@@ -28,7 +28,7 @@ def create_fastapi_app(project_name: str, version: str, description: str) -> Fas
         title=project_name,
         version=version,
         description=description,
-        docs_url=("/authentication" + "/doc") if config.api.show_docs else None,
+        docs_url=("/authentication" + "/docs") if config.api.show_docs else None,
         openapi_url=(config.api.prefix + "/openapi.json") if config.api.show_docs else None,
         container=container,
         dependencies=[Depends(user_token_dep_factory(reusable_oauth2))],
